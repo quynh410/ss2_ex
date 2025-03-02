1ac1c07676b5d6e1dddd5d24f6071d9809cd8447
quynh410 <quynhhhhhh07@gmail.com>
2025-03-02 22:59:49 +0700

    add

--- src/Bai1.java
+++ src/Bai1.java
@@ -1,17 +1,15 @@
 public class Bai1 {
+
     public static void main(String[] args) {
-        int soNguyen = 10;
-        float soThuc = 5.75f;
-        char kyTu = 'Q';
-        String chuoi = "Hello, Rikkei Academy!";
-        int tongHaiSoNguyen = soNguyen + 15;
-        float banKinh = 7.5f;
-        double dienTichHinhTron = Math.PI * Math.pow(banKinh, 2);
-        System.out.println("Giá trị của biến int: " + soNguyen);
-        System.out.println("Giá trị của biến double: " + soThuc);
-        System.out.println("Giá trị của biến char: " + kyTu);
-        System.out.println("Giá trị của biến String: " + chuoi);
-        System.out.println("Tổng hai số nguyên: " + tongHaiSoNguyen);
-        System.out.println("Diện tích hình tròn với bán kính " + banKinh + " là: " + dienTichHinhTron);
+            int a = 10;
+            float db = 3.14f;
+            char ch = 'A';
+            String str = "Hello!";
+
+            // In kết quả ra màn hình
+            System.out.println("Giá trị của biến a: " + a);
+            System.out.println("Giá trị của biến db: " + db);
+            System.out.println("Giá trị của biến ch: " + ch);
+            System.out.println("Giá trị của biến str: " + str);
     }
 }
